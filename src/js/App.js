b1b00585143759a5083d96290045496854f27b82
NazarovAn <naatonchrome@gmail.com>
2020-11-11 13:09:26 +0300

    Start page done

---
+++ src/js/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
+import Start from './pages/index';
+import Slider from './pages/slider';
+
+export default function App() {
+    return (
+      <Router>
+          <Switch>
+            <Route path="/slider">
+                <Slider />
+            </Route>
+            <Route path="/">
+                <Start />
+            </Route>
+          </Switch>
+      </Router>
+    );
+  }
---
+++ src/js/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Start() {
+    return (
+        <div className='slider_container'>
+            <h1 className='slider_heading'>Hello</h1>
+            <Link to='/slider' className='slider_button'>Слайдер</Link>
+        </div>
+    )
+}
